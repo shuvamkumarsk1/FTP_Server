9c9d9e4992a925914d3d5f2d1a50a3f95ebfd6e6
Shuvam Kumar <35462685+shuvamkumarsk1@users.noreply.github.com>
2019-03-02 00:55:57 +0530

    Create ftpscript.sh

---
+++ ftpscript.sh
@@ -0,0 +1,6 @@
+echo "# FTP_Server" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/shuvamkumarsk1/FTP_Server.git
+git push -u origin master
